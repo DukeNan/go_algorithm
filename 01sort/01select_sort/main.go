b440be363f83b2c5784280061577e8b840167636
shaun <dukenan006@163.com>
2022-03-01 16:48:27 +0800

    feat(sort): 添加插入排序算法

--- 01sort/01select_sort/main.go
+++ 01sort/01select_sort/main.go
@@ -1,3 +1,19 @@
+/*
+ * @Author: shaun
+ * @Date: 2022-03-01 15:13:36
+ * @Last Modified by: shaun
+ * @Last Modified time: 2022-03-01 15:35:55
+
+选择排序算法
+
+基本思想：
+
+首先在未排序的数列中找到最小(or最大)元素，
+然后将其存放到数列的起始位置；
+接着，再从剩余未排序的元素中继续寻找最小(or最大)元素，
+然后放到已排序序列的末尾。以此类推，直到所有元素均排序完毕。
+*/
+
 package main
 
 import "fmt"
@@ -5,8 +21,11 @@ import "fmt"
 func SelectSort(data []int) {
 	llen := len(data)
 	for i := 0; i < llen; i++ {
+		// 初始最小值
 		tmp := data[i]
+		// 记录剩余未排序部分最小元素的下标
 		flag := i
+		// 交换剩余未排序部分最小元素的位置
 		for j := i + 1; j < llen; j++ {
 			if data[j] < tmp {
 				tmp = data[j]
@@ -17,10 +36,13 @@ func SelectSort(data []int) {
 			data[flag] = data[i]
 			data[i] = tmp
 		}
+		fmt.Printf("i: %v, data:%v\n", i, data)
 	}
+
 }
 func main() {
-	data := []int{5, 4, 8, 7, 6, 0, 1, 3, 2}
+	data := []int{13, 65, 97, 76, 38, 27, 49}
+	// data := []int{20}
 	SelectSort(data)
 	fmt.Printf("data: %v\n", data)
 
---
+++ 01sort/02insert_sort/main.go
@@ -0,0 +1,45 @@
+/*
+ * @Author: shaun
+ * @Date: 2022-03-01 16:11:33
+ * @Last Modified by: shaun
+ * @Last Modified time: 2022-03-01 16:46:49
+
+ 插入排序
+
+基本思想：
+它的工作原理是通过构建有序序列，
+对于未排序数据，在已排序序列中从后向前扫描，
+找到相应位置并插入。插入排序在实现上，
+在从后向前扫描过程中，
+需要反复把已排序元素逐步向后挪位，
+为最新元素提供插入空间。
+
+*/
+package main
+
+import "fmt"
+
+func InsertSort(data []int) {
+	length := len(data)
+	if length <= 1 {
+		return
+	}
+	for i := 1; i < length; i++ {
+		// 存放当前需要排序的元素值
+		back := data[i]
+		j := i - 1
+		// 已排序部分每次向左移动，小于back向右移动一位
+		for j >= 0 && back < data[j] {
+			data[j+1] = data[j]
+			j--
+		}
+		// 将back值放到已排序部分最小位置
+		data[j+1] = back
+	}
+}
+
+func main() {
+	data := []int{7, 3, 19, 40, 4, 7, 1}
+	InsertSort(data)
+	fmt.Printf("data: %v\n", data)
+}
